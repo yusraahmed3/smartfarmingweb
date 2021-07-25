0130ff905aefc593c3bc3783c40efec0c62cd981
Yusra <yusraa190@gmail.com>
2021-07-25 22:41:08 +0300

    rejected requests page

--- src/components/RejectedRequests.js
+++ src/components/RejectedRequests.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import './RejectedRequests.css'
+import EditIcon from '@material-ui/icons/Edit';
 
 function RejectedRequests() {
     return (
 <>
             <Sidebar/>
             <div className="rejectedcss">
-            Lists of rejected lists
+            <div className="pagetitle">
+                <h2>Rejected Requests</h2>
+            </div>
+            <div>
+                <ul className="activelist">
+                    <li className="listitems">
+                       <div id="titles">Rejected Request 1</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Rejected Request 2</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Rejected Request 3</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Rejected Request 4</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Rejected Request 5</div><div id="icons"><EditIcon/></div>
+                    </li>
+                </ul>
+            </div>
         </div>
         </>
     )
