b950468ca1615d72e634bb4b6cef8a56123e7c42
Yusra <yusraa190@gmail.com>
2021-07-25 14:55:53 +0300

    profile avatar

---
+++ src/components/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import './Avatar.css'
+
+function Avatar() {
+    return (
+        <div className="avatarcss">
+            <AccountCircleIcon/>
+        </div>
+    )
+}
+
+export default Avatar
