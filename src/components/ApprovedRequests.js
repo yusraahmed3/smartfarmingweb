dfb89eb36241990f98341a5e09e9a8c8976beebd
Yusra <yusraa190@gmail.com>
2021-07-25 22:36:21 +0300

    approved requests page

--- src/components/ApprovedRequests.js
+++ src/components/ApprovedRequests.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import './ApprovedRequests.css'
-
+import EditIcon from '@material-ui/icons/Edit';
 function ApprovedRequests() {
     return (
 <>
             <Sidebar/>
             <div className="approvedcss">
-            Lists of approved requests
+            <div className="pagetitle">
+                <h2>Approved Requests</h2>
+            </div>
+            <div>
+                <ul className="activelist">
+                    <li className="listitems">
+                       <div id="titles">Approved Request 1</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Approved Request 2</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Approved Request 3</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Approved Request 4</div><div id="icons"><EditIcon/></div>
+                    </li>
+                    <li className="listitems">
+                    <div id="titles">Approved Request 5</div><div id="icons"><EditIcon/></div>
+                    </li>
+                </ul>
+            </div>
         </div>
         </>
     )
