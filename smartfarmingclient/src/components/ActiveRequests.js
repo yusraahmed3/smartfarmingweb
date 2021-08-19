4cd7d54a21e5a9f8fa055c9b972f8798b907fada
Yusra A <henockyeyoni@gmail.com>
2021-08-19 10:05:27 +0300

    request functionality fix

--- smartfarmingclient/src/components/ActiveRequests.js
+++ smartfarmingclient/src/components/ActiveRequests.js
@@ -1,73 +1,121 @@
 import React, { Component } from "react";
 import Sidebar from "./Sidebar";
-import {Link} from 'react-router-dom'
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import axios from "axios";
 import "./ActiveRequests.css";
+import ApprovedRequests from './ApprovedRequests'
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import ThumbDownIcon from "@material-ui/icons/ThumbDown";
 import CircularProgress from "@material-ui/core/CircularProgress";
+
+function Requests({ request, index, approveRequest, rejectRequest }) {
+  return (
+    <ul className="activelist">
+      <li className="listitems" key={index} >
+        <div className="title">{request.firstname}</div>
+         <div className="icon1">
+          <button onClick={() => approveRequest(request)}>
+            <ThumbUpIcon />
+          </button>
+        </div>
+        <div className="icon2">
+          <button onClick={() => rejectRequest(request) } >
+            <ThumbDownIcon />
+          </button>
+        </div> 
+      </li>
+    </ul>
+  );
+}
 
 class ActiveRequests extends Component {
   constructor(props) {
     super(props);
     this.state = {
       requests: [],
+      request: [],
       loading: true,
     };
   }
 
-  handleApproveButton = () => {
-    const url = 'http://localhost:4000/approved';
-    this.state.requests.map((req) => (
+ handleApproveButton = (req) => {
+    console.log("Inside approve method");
+    const url = "http://localhost:4000/approved";
     axios({
-      method: 'post',
+      method: "post",
       url: url,
       data: {
-        status: 'approved',
+        status: "approved",
         firstname: req.firstname,
         lastname: req.lastname,
         phoneno: req.phoneno,
         instname: req.instname,
         email: req.email,
         password: req.password,
-        message: req.message
+        message: req.message,
+      },
+    }).then(
+      (response) => {
+        console.log("Request approved!");
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
       }
-  })
-  .then((response) => {
-      alert("Request approved!")
-      console.log(response);
-    }, (error) => {
-      console.log(error);
-    })
-    ))
-  }
+    ).then(
+      this.setState(axios.delete(`http://localhost:4000/requests/${req._id}`))
+      
+    );
+  };
 
-  handleRejectButton = () => {
-    const url = 'http://localhost:4000/rejected';
-    this.state.requests.map((req) => (
+  // detailPage(){
+  //   return(
+  //     <ApprovedRequests />
+  //     // <div>
+      //   <Sidebar />
+      //   <ul>
+      //     <li>
+      //       {req.firstname}
+      //     </li>
+      //     <li>
+      //       {req.lastname}
+      //     </li>
+      //     <li>
+      //       {req.instname}
+      //     </li>
+      //   </ul>
+      // </div>
+  //   )
+  // }
+  handleRejectButton = (req) => {
+    console.log("Inside reject method");
+    const url = "http://localhost:4000/rejected";
     axios({
-      method: 'post',
+      method: "post",
       url: url,
       data: {
-        status: 'rejected',
+        status: "rejected",
         firstname: req.firstname,
         lastname: req.lastname,
         phoneno: req.phoneno,
         instname: req.instname,
         email: req.email,
         password: req.password,
-        message: req.message
+        message: req.message,
+      },
+    }).then(
+      (response) => {
+        console.log("Request rejected!");
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
       }
-  })
-  .then((response) => {
-      alert("Request rejected!")
-      console.log(response);
-    }, (error) => {
-      console.log(error);
-    })
-    ))
-
-  }
+    ).then(
+      axios.delete(`http://localhost:4000/requests/${req._id}`)
+    );
+    
+  };
 
   async componentDidMount() {
     const url = "http://localhost:4000/requests";
@@ -96,21 +144,9 @@ class ActiveRequests extends Component {
             <div className="pagetitle">
               <h3>Active Requests</h3>
             </div>
-            <div>
-              <ul className="activelist">
-                {this.state.requests.map((req) => (
-                  <li className="listitems" key={req._id}>
-                   <div className="title">{req.instname}</div>
-                    <div className="icon1">
-                    <button onClick={this.handleApproveButton}><ThumbUpIcon /></button>  
-                    </div>
-                    <div className="icon2">
-                     <button onClick={this.handleRejectButton}><ThumbDownIcon /></button> 
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
+            {this.state.requests.map((req, index) => (
+              <Requests key={index} index={index} request={req} approveRequest={this.handleApproveButton} rejectRequest={this.handleRejectButton}/>
+            ))}
           </div>
         </>
       );
--- smartfarmingclient/src/components/ApprovedRequests.js
+++ smartfarmingclient/src/components/ApprovedRequests.js
@@ -44,7 +44,7 @@ class ApprovedRequests extends Component {
               <ul className="activelist">
                 {this.state.approvedRequests.map((req) => (
                   <li className="listitems" key={req._id}>
-                    <div id="titles">{req.instname}</div>
+                    <div id="titles">{req.firstname}</div>
                     <div id="icons">
                       <EditIcon />
                     </div>
