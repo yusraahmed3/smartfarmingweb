2e17bd7140c1da388e435c6eec73472c6f9ede74
Yusra <netsasew55@gmail.com>
2021-09-10 17:07:38 +0300

    User profile

--- smartfarmingclient/src/components/User.js
+++ smartfarmingclient/src/components/User.js
@@ -1,25 +1,53 @@
-import React from 'react'
-import UserSidebar from './UserSidebar'
-import './User.css'
-import SearchBar from './SearchBar'
-import Cards from './Cards'
-function User() {
+import React, { useLayoutEffect, useState } from "react";
+import UserSidebar from "./UserSidebar";
+import "./User.css";
+import SearchBar from "./SearchBar";
+import Cards from "./Cards";
+import axios from "axios";
 
-  
-    return (
-        <>
-        <UserSidebar/>
-        <div className="position">
+function User() {
+  const [image, setImage] = useState("");
+
+  useLayoutEffect(() => {
+    axios({
+      url: "http://localhost:4000/users/user",
+      method: "get",
+      headers: {
+        "x-access-token": localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        console.log(res.data.user.idimg);
+        setImage(res.data.user.idimg);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <UserSidebar image={image} />
+      <div className="position">
         <div className="pagetitle">
-             <h3>Dashboard</h3>
-            </div>
-            <SearchBar/>
-            <br/><br/>
-            <Cards location="Location 1" temperature="57" humidity="27" moisture="09"/>
-            <Cards location="Location 2" temperature="31.5" humidity="20" moisture="12"/>
+          <h3>Dashboard</h3>
         </div>
-        </>
-    )
+        <SearchBar />
+        <br />
+        <br />
+        <Cards
+          location="Location 1"
+          temperature="57"
+          humidity="27"
+          moisture="09"
+        />
+        <Cards
+          location="Location 2"
+          temperature="31.5"
+          humidity="20"
+          moisture="12"
+        />
+      </div>
+    </>
+  );
 }
 
-export default User
+export default User;
